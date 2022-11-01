fae02fde804321cb0bd0880ec9b75bf34b37cc71
AnwarFA <anwaralaiban123@gmail.com>
2022-11-02 00:00:10 +0300

    Done the task

--- flights/views.py
+++ flights/views.py
@@ -1,10 +1,12 @@
 from datetime import datetime
-
+from rest_framework.generics import CreateAPIView
 from rest_framework import generics
-
+from .serializers import RegisterSerializer
 from flights import serializers
 from flights.models import Booking, Flight
 
+class RegisterAPIView(CreateAPIView):
+    serializer_class = RegisterSerializer
 
 class FlightsList(generics.ListAPIView):
     queryset = Flight.objects.all()
